1f2a5631a0906b0ce4df7719930085068f7fe4f0
mlz000 <njumlz@gmail.com>
2017-08-25 21:26:22 +0800

    .

---
+++ leetcode/13.py
@@ -0,0 +1,7 @@
+class Solution(object):
+    def romanToInt(self, s):
+        d = {'M' : 1000, 'D' : 500, 'C' : 100, 'L' : 50, 'X' : 10, 'V' : 5, 'I' : 1}
+        res, p = 0, 'I'
+        for c in s[::-1]:
+            res, p = res - d[c] if d[c] < d[p] else res + d[c], c
+        return res
